3b400235329a0dd382ade1062ed709166738aad1
liangys <liangys@LiangdeMacBook-Pro.local>
2023-05-24 10:24:43 +0800

    readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Adaptive Plasticity Improvement for Continual Learning
+Code For CVPR'2023 paper "[Adaptive Plasticity Improvement for Continual Learning]
+
 ## Requisite
 
 This code is implemented in PyTorch, and we have tested the code under the following environment settings:
@@ -12,5 +15,5 @@ This code is implemented in PyTorch, and we have tested the code under the follo
 bash script/perform.sh Your_data_path Your_CUDA_id
 ```
 
-We would like to thank authors of the following repositories (from which we borrowed code). </br>
+We would like to thank the authors of the following repositories (from which we borrowed code). </br>
 * https://github.com/danruod/FS-DGPM
--- script/perform.sh
+++ script/perform.sh
@@ -3,8 +3,8 @@
 n_runs=5
 
 CIFAR20='--dataset cifar100_20 --n_tasks 20 --pc_valid 0.05 --batch_size 64 --test_batch_size 64 --n_epochs 200 --cuda --earlystop'
-CSUPER='--dataset cifar100_superclass --data_path /home/liangys/data --pc_valid 0.05 --batch_size 64 --test_batch_size 64 --n_epochs 50 --cuda --earlystop'
-FIVED='--dataset five_datasets --samples_per_task -1 --data_path /home/liangys/data --n_tasks 5 --pc_valid 0.05 --batch_size 64 --test_batch_size 64 --n_epochs 100 --cuda --earlystop --lr_min 1e-3 --lr_patience 5 --lr_factor 3'
+CSUPER='--dataset cifar100_superclass --pc_valid 0.05 --batch_size 64 --test_batch_size 64 --n_epochs 50 --cuda --earlystop'
+FIVED='--dataset five_datasets --samples_per_task -1 --n_tasks 5 --pc_valid 0.05 --batch_size 64 --test_batch_size 64 --n_epochs 100 --cuda --earlystop --lr_min 1e-3 --lr_patience 5 --lr_factor 3'
 
 # 20-Split CIFAR100
 CUDA_VISIBLE_DEVICES=$2 python main_api.py $CIFAR20 --n_runs $n_runs --data_path $1 --model api --lr 0.01 --model_arch alexnet --model_size 1.0 --expt_name api --cuda_enabled 1 --alpha 10 --step 0.5 
